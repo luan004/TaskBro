39e0b83b1ed4500f2111b9ce2633265873bbc671
Luan Gabriel <luan.gabriel.ifpr@gmail.com>
2024-06-26 21:55:16 -0300

    feat: tailwind deploy

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import '../assets/styles/Login.css';
+
+const Login = () => {
+
+    return (
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+
+export default Login;
